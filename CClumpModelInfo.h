6bde71c377a91759d6ea77e4175da01c466da6aa
JohnPeriaX <johnperia.job@gmail.com>
2025-05-17 17:34:30 +0700

    Add files via upload

---
+++ CClumpModelInfo.h
@@ -0,0 +1,21 @@
+//
+// Created by JohnPeriaX on 05.17.2025.
+//
+
+#pragma once
+
+#include "PedModelInfo.h"
+#include "AtomicModelInfo.h"
+#include "VehicleModelInfo.h"
+#include "game/Core/Store.h"
+#include "game/constants.h"
+
+class CModelInfo {
+public:
+    static constexpr int32_t NUM_CLUMP_MODEL_INFOS = 1000; // จำนวนสูงสุดของโมเดล Clump
+    static inline CStore<CClumpModelInfo, NUM_CLUMP_MODEL_INFOS> ms_clumpModelInfoStore; // คลังข้อมูลโมเดล Clump
+
+    // ฟังก์ชันสมาชิก
+    static void injectHooks(); // ติดตั้งฮุคต่างๆ
+
+};
